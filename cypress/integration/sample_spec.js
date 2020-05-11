956a13cdfd4e2fc5da15e7dbd3fa8a7437527f69
Garrett Bogart <bogartgarrett@gmail.com>
2020-05-11 13:56:40 -0600

    visited the sink

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-        expect(true).to.equal(true)
+describe('Kitchen Sink', () => {
+    it('Visits the Kitchen Sink', () =>{
+        cy.visit('https://example.cypress.io')
     })
 })
