2d23f3e04391aa683e02447d1d821f69327e249e
Garrett Bogart <bogartgarrett@gmail.com>
2020-05-11 14:03:51 -0600

    finished basic test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,11 @@
 describe('Kitchen Sink', () => {
     it('Visits the Kitchen Sink', () =>{
         cy.visit('https://example.cypress.io')
+        cy.contains('type').click()
+        cy.url().should('include', '/commands/actions')
+
+        cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value', 'fake@email.com')
     })
 })
